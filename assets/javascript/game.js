5c11085929a4c671816c09bd211dc63fa8eb6936
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-13 17:58:17 -0600

    let's raise the bar

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,26 @@ var winsCount;
 var lossesCount;
 var currentTotal;
 var targetNumber
+var bismuth;
+var pastel;
+var ferro;
+var green;
+
+var bismuth = bismuth();
+var pastel = pastel();
+var ferro = ferro();
+var green = green();
 
 
+
+
+
+
+var targetNumber = generateTarget();
 
 
 
 console.log(targetNumber);
-
-
-
-
-	function generateTarget(){
-		var number = Math.floor((Math.random() * 120) + 19);
-		$("#target").text(number);
-
-	};
-var targetNumber = generateTarget();
-console.log(targetNumber)
 
 
 //FUNCTIONS
@@ -37,12 +40,51 @@ console.log(targetNumber)
 // how to design a function to  keep goinggggg... lost train of thought
 // run functions to tabulate new target numbers, new crystal values, reset cumulative
 //and alter wins/losses++ with a single if/else.
+function generateTarget(){
+		var number = Math.floor((Math.random() * 120) + 19);
+		$("#target").text(number);
+		console.log(number);
+		return number;
+	};
 
+//consider finding a way to make this a for-loop, 
+//executed at the whatever trigger, 
+//also maybe it should be part of an object
+function bismuth(){
+		var number = Math.floor((Math.random() * 12) + 1);
+		$("#target").text(number);
+		console.log(number);
+		return number;
+	};
 
+console.log(bismuth);
 
+function pastel(){
+		var number = Math.floor((Math.random() * 12) + 1);
+		$("#target").text(number);
+		console.log(number);
+		return number;
+	};
 
+console.log(pastel);
 
+function ferro(){
+		var number = Math.floor((Math.random() * 12) + 1);
+		$("#target").text(number);
+		console.log(number);
+		return number;
+	};
 
+console.log(ferro);
+
+function green(){
+		var number = Math.floor((Math.random() * 12) + 1);
+		$("#target").text(number);
+		console.log(number);
+		return number;
+	};
+
+console.log(green);
 
 
 
