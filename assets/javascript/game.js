72ce1cffc636397d56fc8cf7e82f7f45d3510f01
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-13 16:24:23 -0600

    Making sure everything is up to date

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,41 @@
 
 //GLOBAL VARIABLES
 //---------------------------------------------------
-//individual variables
+// individual variables
 // consider which variables need to actually contain functions
 //
+var winsCount;
+var lossesCount;
+var currentTotal;
+var targetNumber
 
 
 
 
+
+console.log(targetNumber);
+
+
+
+
+	function generateTarget(){
+		var number = Math.floor((Math.random() * 120) + 19);
+		$("#target").text(number);
+
+	};
+var targetNumber = generateTarget();
+console.log(targetNumber)
 
 
 //FUNCTIONS
 //-----------------------------------------------------
 // add clicked value to accrueing total
 // counter to tabulate 
-//individual functions, each stored as a variable, that performs
-//the function of adding a total to the accrueing total
-//how to design a function to  keep goinggggg... lost train of thought
-
+// individual functions, each stored as a variable, that performs
+// the function of adding a total to the accrueing total
+// how to design a function to  keep goinggggg... lost train of thought
+// run functions to tabulate new target numbers, new crystal values, reset cumulative
+//and alter wins/losses++ with a single if/else.
 
 
 
@@ -30,3 +48,12 @@
 
 //MAIN PROCESSES
 //----------------------------------------------------
+
+
+
+//-------------------------
+//BONUS
+// have another panel entry that 
+// displays the probability of hitting the
+// target number with the individual numbers (still hidden)
+// that have been assigned to the crystals.
