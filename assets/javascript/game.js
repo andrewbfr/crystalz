6c8bcfa31e324489858ca26a1297795675ca63ca
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-12 18:19:36 -0600

    styling and, again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,9 @@
 
 //GLOBAL VARIABLES
 //---------------------------------------------------
-
-
+//individual variables
+// consider which variables need to actually contain functions
+//
 
 
 
@@ -12,6 +13,11 @@
 
 //FUNCTIONS
 //-----------------------------------------------------
+// add clicked value to accrueing total
+// counter to tabulate 
+//individual functions, each stored as a variable, that performs
+//the function of adding a total to the accrueing total
+//how to design a function to  keep goinggggg... lost train of thought
 
 
 
